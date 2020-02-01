ea5ce918fc9bbe3f6095fde7b069fc863d5796f6
Aliaksandr Zahatski <zag@cpan.org>
2020-02-01 18:12:41 +0300

    add error handling

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css';
 //@ts-ignore
 import { toHtml } from 'pod6'
 
-const { useState } = React;
+const { useState, useRef, useEffect } = React;
 const makeHtml = (text:string) => {
   try {
    return toHtml().run(text)
@@ -16,21 +16,52 @@ or please, fill issue <a target="_blank" href="https://github.com/zag/js-pod6/is
   }
 }
 const App: React.FC = () => {
-  const [text, updateText] = useState('');
+  const [text, updateText] = useState('')
+  const [marks, updateMarks] = useState([])
+
   var options = {
     lineNumbers: true,
  };
+
+ const inputEl = useRef(null)
+ const result = makeHtml(text)
+ useEffect(() => {
+   //@ts-ignore
+   let cm = inputEl.current.getCodeMirror()
+   //@ts-ignore
+  marks.forEach(marker => marker.clear())
+  let cmMrks:Array<never> = []
+  result.errors.map((loc:any)=>{
+    // @ts-ignore
+    let from = {line: loc.start.line-1, ch: loc.start.column-1 - (loc.start.offset === loc.end.offset)};
+    let to = {line: loc.end.line-1, ch: loc.end.column-1};
+    cmMrks.push(
+                //@ts-ignore
+                cm.markText(
+                    from,
+                    to, 
+                    {
+                      className: 'syntax-error',
+                      title: ';data.error.message',
+                      css: "color : red"
+                    }
+                )
+                    
+    )
+  })
+  updateMarks(cmMrks)
+  
+},[text])
   return (
     <div className="App">
       <h1 className="title">pod6 to html</h1>
       <div className="layout">
         <div className="layout__panel flex flex--row">
           <div className="layout__panel left">
-          <CodeMirror value={text} autoFocus onChange={updateText} options={options} />
+          <CodeMirror value={text} ref={inputEl} autoFocus onChange={updateText} options={options} />
            </div>
-
           <div className="layout__panel right"
-          dangerouslySetInnerHTML={{__html: makeHtml(text)}} ></div>
+          dangerouslySetInnerHTML={{__html: result}} ></div>
         </div>
       </div>
     </div>
