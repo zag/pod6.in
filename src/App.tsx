0531713df1ac08f19ab5e9177e9f751c1f048c98
Aliaksandr Zahatski <github.send@zag.ru>
2020-04-26 19:01:36 +0300

    update pod6 to 0.0.19

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,17 @@ const deftext =
 =for code :allow(B)
 This I<is> a B<text>
 
+  =alias PROGNAME    Earl Irradiatem Evermore
+  =alias VENDOR      4D Kingdoms
+  =alias TERMS_URLS  =item L<http://www.4dk.com/eie>
+  =                  =item L<http://www.4dk.co.uk/eie.io/>
+  =                  =item L<http://www.fordecay.ch/canttouchthis>
+  
+  The use of A<PROGNAME> is U<subject> to the terms and conditions
+  laid out by A<VENDOR>, as specified at:
+  
+       A<TERMS_URLS>
+
 =para
 Use a C<for> loop instead.N<The Raku C<for> loop is far more
 powerful than its Perl 5 predecessor.> Preferably with an explicit
