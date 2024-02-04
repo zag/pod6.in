af7e68b95d2f804ec34f26c870e78de1fe75992f
Aliaksandr Zahatski <zag@cpan.org>
2024-02-04 19:33:45 +0100

    tidy

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,25 @@
-import React from 'react';
-import Editor, {ConverterResult} from '@podlite/editor-react/esm'
+import React from 'react'
+import Editor, { ConverterResult } from '@podlite/editor-react'
 // @ts-ignore
 import Podlite from '@podlite/to-jsx'
-import toast from "cogo-toast"
-import copy from "copy-to-clipboard"
+import toast from 'cogo-toast'
+import copy from 'copy-to-clipboard'
 import { ImListNumbered, ImMagicWand } from 'react-icons/im'
-import { AiOutlineClear, AiOutlineLink} from 'react-icons/ai'
-import { BsArrowsFullscreen } from "react-icons/bs";
-import { useHistory } from "react-router-dom"
-import { podlite as podlite_core } from "podlite";
+import { AiOutlineClear, AiOutlineLink } from 'react-icons/ai'
+import { BsArrowsFullscreen } from 'react-icons/bs'
+import { useHistory } from 'react-router-dom'
+import { podlite as podlite_core } from 'podlite'
 import { plugin as DiagramPlugin } from '@podlite/diagram'
-import { isNamedBlock} from '@podlite/schema/lib/ast-inerator';
-import { version, parse,  Node } from '@podlite/schema';
-
+import { isNamedBlock } from '@podlite/schema'
+import { version, parse, Node } from '@podlite/schema'
 
 import '@podlite/editor-react/lib/index.css'
-import '../node_modules/codemirror/lib/codemirror.css';
-import 'codemirror/addon/hint/show-hint';
+import '../node_modules/codemirror/lib/codemirror.css'
+import 'codemirror/addon/hint/show-hint'
 
-import './App.css';
+import './App.css'
 
-let deftext = 
-`=head1 Title
+let deftext = `=head1 Title
 =head2 Subtitle
 
 =for code :allow(B)
@@ -39,9 +37,9 @@ This I<is> a B<text>
        A<TERMS_URLS>
 
 =para
-Use a C<for> loop instead.N<The Raku C<for> loop is far more
-powerful than its Perl 5 predecessor.> Preferably with an explicit
-iterator variable.
+Space stations feature hydroponic gardens B<N<Plants grow without soil,
+using mineral nutrient solutions.>>, a necessity for long-term missions.
+
 
 Options B<are>:
 
@@ -73,141 +71,192 @@ I<Table>
 
 =end table
 `
-const { useState,   useEffect } = React;
+const { useState, useEffect } = React
 
 const App1: React.FC = () => {
-    const [showTree ] = useState(false)
-    const [isLineNumbers, setLineNumbers] = useState(false)
-    let history = useHistory()
+  const [showTree] = useState(false)
+  const [isLineNumbers, setLineNumbers] = useState(false)
+  let history = useHistory()
 
-    // URL state management
-    type GetHashStringParams = {p:string, f:boolean}
-    const pushHistory = (args:GetHashStringParams ) => {
-        const result = getHashString(args)
-        if (history) {
-            history.push({hash: result})
-        } 
+  // URL state management
+  type GetHashStringParams = { p: string; f: boolean }
+  const pushHistory = (args: GetHashStringParams) => {
+    const result = getHashString(args)
+    if (history) {
+      history.push({ hash: result })
+    }
+  }
+
+  const getHashString = ({ p: planText, f: isFullScreen }: GetHashStringParams): string => {
+    const params = new URLSearchParams()
+    planText ? params.append('p', planText) : params.delete('p')
+    isFullScreen ? params.append('f', '1') : params.delete('f')
+    return params.toString()
+  }
+
+  const getStateFromHash = (): GetHashStringParams => {
+    if (history && history.location) {
+      const params = new URLSearchParams(history.location.hash)
+      const p = params.get('#p') || params.get('p')
+      const f = Boolean(params.get('f') || params.get('#f'))
+      return { p, f }
+    } else {
+      return { p: null, f: null }
+    }
+  }
+  // URL state management
+
+  let default_text = getStateFromHash().p
+  let isFullscreenByDefault = getStateFromHash().f
+
+  const [query, setQuery] = useState(default_text || deftext)
+
+  const [isFullScreenPreview, setFullScreenPreview] = useState(isFullscreenByDefault)
+
+  // wrap all elements and add line link info
+  const wrapFunction = (node: any, children) => {
+    if (
+      typeof node !== 'string' &&
+      'type' in node &&
+      'location' in node &&
+      node.type === 'block' &&
+      (['para', 'head', 'item', 'table', 'comment', 'nested', 'input', 'output', 'pod', 'caption'].includes(
+        node.name,
+      ) ||
+        isNamedBlock(node.name))
+    ) {
+      //@ts-ignore
+      const line = node.location.start.line
+      return (
+        <div key={line} className="line-src" data-line={line} id={`line-${line}`}>
+          {children}
+        </div>
+      )
+    } else {
+      return children
+    }
+  }
+
+  const onConvertSource = (text: string): ConverterResult => {
+    console.log({ version })
+
+    if (showTree) {
+      const previewCode = (
+        <div className=" right">
+          <pre>
+            <code className="right" style={{ textAlign: 'left' }}>
+              {JSON.stringify(parse(text), null, 2)}
+            </code>
+          </pre>
+        </div>
+      )
+      return { result: previewCode }
     }
 
-    const getHashString = ({p:planText, f:isFullScreen}:GetHashStringParams):string => {
-        const params = new URLSearchParams()
-        planText ? params.append("p", planText) : params.delete("p")
-        isFullScreen ? params.append("f", "1") : params.delete("f")
-        return params.toString()
-    }
-    
-    const getStateFromHash = ():GetHashStringParams => {
-        if (history && history.location ) {
-        const params = new URLSearchParams(history.location.hash)
-        const p = params.get('#p') || params.get('p')
-        const f = Boolean ( params.get('f') ||params.get('#f') )
-        return  { p, f}
-        } else { return {p:null, f:null}}
-    }
-   // URL state management
+    pushHistory({
+      p: text,
+      f: isFullScreenPreview,
+    })
 
-    let default_text = getStateFromHash().p
-    let isFullscreenByDefault = getStateFromHash().f
+    setChanged(true)
 
-    const [query, setQuery] = useState(default_text || deftext)
+    let podlite = podlite_core({ importPlugins: true }).use({
+      Diagram: DiagramPlugin,
+    })
+    let tree = podlite.parse(text)
+    const asAst = podlite.toAstResult(tree)
 
-    const [isFullScreenPreview, setFullScreenPreview] = useState(isFullscreenByDefault)
+    //@ts-ignore
+    return { result: <Podlite wrapElement={wrapFunction} tree={asAst} />, errors: asAst.errors }
+  }
 
-    // wrap all elements and add line link info
-    const wrapFunction = (node: any, children) => {
-        if (typeof node !== 'string' && 'type' in node && 'location' in node && node.type === 'block' && 
-        (['para','head','item', 'table', 'comment','nested','input','output','pod', 'caption'].includes(node.name)
-        || isNamedBlock(node.name))) {
-            //@ts-ignore
-            const line = node.location.start.line
-            return <div key={line} className="line-src" data-line={line} id={`line-${line}`}>{children}</div>
-        } else {
-            return children
-        }
-    }
+  useEffect(() => {
+    pushHistory({
+      p: getStateFromHash().p,
+      f: isFullScreenPreview,
+    })
+  }, [isFullScreenPreview])
 
-    const onConvertSource = (text:string):ConverterResult=>{
-        
-        if (showTree ) {
-            const previewCode = <div  className=" right"><pre><code className="right" style={{textAlign:"left"}}>{JSON.stringify(parse(text), null, 2)}</code></pre></div>
-            return {result:previewCode}
-        }
-        
-        pushHistory({
-            p:text,
-            f: isFullScreenPreview
-        })
+  // useEffect(() => {
+  //     const params = new URLSearchParams()
+  //     if (query) {
+  //       params.append("p", query)
+  //     } else {
+  //       params.delete("p")
+  //     }
+  //     if (history) {
+  //      history.push({hash: params.toString()})
+  //     }
+  //   }, [query, history])
 
-        setChanged(true)
+  // reset text to template
+  const [isChanged, setChanged] = useState(false)
+  const [isAutocompleteOn, setAutocomplete] = useState(true)
 
-        let podlite = podlite_core({ importPlugins: true }).use({
-            Diagram: DiagramPlugin,
-          });
-          let tree = podlite.parse(text);
-          const asAst = podlite.toAstResult(tree);
-
-        //@ts-ignore
-        return { result : <Podlite wrapElement={wrapFunction} tree={asAst} />, errors:asAst.errors }
-
-    }
-        
-    useEffect(() => {
-        pushHistory({
-            p:getStateFromHash().p,
-            f: isFullScreenPreview
-        })
-      }, [isFullScreenPreview])
-    
-    // useEffect(() => {
-    //     const params = new URLSearchParams()
-    //     if (query) {
-    //       params.append("p", query)
-    //     } else {
-    //       params.delete("p")
-    //     }
-    //     if (history) {
-    //      history.push({hash: params.toString()})
-    //     }
-    //   }, [query, history])
-
-    // reset text to template
-    const [isChanged, setChanged ] = useState(false)
-    const [isAutocompleteOn, setAutocomplete ] = useState(true)
-
-    const copyToClipboard = () => {
-        copy(document.location.href);
-        toast.success(`Url to this text copied to clipboard`, {
-          position: "top-center"
-        });
-      };
-    return   <div className="App">
-    <div style={{  'textAlign': 'left',
-                    'margin': '0em 1em',
-                    }}>
-        <h1 className="title"><a href='https://podlite.org' target='_blank'>Podlite</a> online editor (<a target="_blank" rel="noopener noreferrer" href="https://github.com/podlite/podlite/tree/main/packages/podlite-schema">{version}</a>)</h1>
-        <ImListNumbered className={ isLineNumbers ? 'iconOn' : 'iconOff' } title="toggle line numbers" onClick={()=>setLineNumbers(!isLineNumbers)}/>
-        <BsArrowsFullscreen className={ isFullScreenPreview ? 'iconOn' : 'iconOff' } title="Toggle fullscreen" onClick={()=>setFullScreenPreview(!isFullScreenPreview)}/>
-        <AiOutlineClear className={ isChanged ? 'iconOn' : 'iconOff' } title="Reset text to template" onClick={()=>{ 
+  const copyToClipboard = () => {
+    copy(document.location.href)
+    toast.success(`Url to this text copied to clipboard`, {
+      position: 'top-center',
+    })
+  }
+  return (
+    <div className="App">
+      <div style={{ textAlign: 'left', margin: '0em 1em' }}>
+        <h1 className="title">
+          <a href="https://podlite.org" target="_blank">
+            Podlite
+          </a>{' '}
+          online editor (
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/podlite/podlite/tree/main/packages/podlite-schema"
+          >
+            {version}
+          </a>
+          )
+        </h1>
+        <ImListNumbered
+          className={isLineNumbers ? 'iconOn' : 'iconOff'}
+          title="toggle line numbers"
+          onClick={() => setLineNumbers(!isLineNumbers)}
+        />
+        <BsArrowsFullscreen
+          className={isFullScreenPreview ? 'iconOn' : 'iconOff'}
+          title="Toggle fullscreen"
+          onClick={() => setFullScreenPreview(!isFullScreenPreview)}
+        />
+        <AiOutlineClear
+          className={isChanged ? 'iconOn' : 'iconOff'}
+          title="Reset text to template"
+          onClick={() => {
             // setChanged(false); // setQuery(deftext);  // updateHistory(deftext);
-            if ( window.confirm('Are you really sure?') ) { document.location.assign('/') }
-            }}/>
-        <AiOutlineLink onClick={ copyToClipboard}  className='iconOn' title="Copy url to this text"/>
-        <ImMagicWand onClick={ ()=>setAutocomplete(!isAutocompleteOn)}  className={ isAutocompleteOn ? 'iconOn' : 'iconOff' } title="toggle autocomplete for directives"/>
-        
-    </div>
+            if (window.confirm('Are you really sure?')) {
+              document.location.assign('/')
+            }
+          }}
+        />
+        <AiOutlineLink onClick={copyToClipboard} className="iconOn" title="Copy url to this text" />
+        <ImMagicWand
+          onClick={() => setAutocomplete(!isAutocompleteOn)}
+          className={isAutocompleteOn ? 'iconOn' : 'iconOff'}
+          title="toggle autocomplete for directives"
+        />
+      </div>
 
-    <Editor 
-        isLineNumbers= {isLineNumbers}
-        isPreviewModeEnabled = {isFullScreenPreview}
-        isControlled = {true}
+      <Editor
+        isLineNumbers={isLineNumbers}
+        isPreviewModeEnabled={isFullScreenPreview}
+        isControlled={true}
         isAutoComplete={isAutocompleteOn}
-        content={query}  
-        onChangeSource = { (content:string)=>{
-            setQuery(content)
-        } }
-        sourceType= { 'pod6' }
-        onConvertSource = { onConvertSource }
-    /> 
+        content={query}
+        onChangeSource={(content: string) => {
+          setQuery(content)
+        }}
+        sourceType={'pod6'}
+        onConvertSource={onConvertSource}
+      />
     </div>
+  )
 }
-export default App1;
+export default App1
--- src/ast-to-react.tsx
+++ src/ast-to-react.tsx
@@ -1,128 +1,150 @@
 //ts-nocheck
 import React from 'react'
-import ReactDOMServer from 'react-dom/server';
-import { useMemo, useState, useCallback, useRef, useEffect }  from 'react'
-import {isValidElementType} from  'react-is'
-import { version, toAny, parse } from '@podlite/schema';
-import path from 'path';
+import ReactDOMServer from 'react-dom/server'
+import { useMemo, useState, useCallback, useRef, useEffect } from 'react'
+import { isValidElementType } from 'react-is'
+import { version, toAny, parse } from '@podlite/schema'
+import path from 'path'
 // import { parseToAst } from './make-ast';
-let i_key_i = 0;
+let i_key_i = 0
 const createElement = React.createElement
 
 const mapToReact = {
-    // ':para' : ({children, line})=><p linesrc={line}>{children}</p>,
-    // ':blankline': ({children})=><br/>,
-    ':blankline': ({children})=>null,
-    
-    // 'para':({line,children})=>React.createElement(`p`,{'data-line':line, className:"line-src", id:`line-${line}`}, children),
-    'para':({line,children})=>(<p data-line={line} className="line-src" id={`line-${line}`}>{children}</p>), 
+  // ':para' : ({children, line})=><p linesrc={line}>{children}</p>,
+  // ':blankline': ({children})=><br/>,
+  ':blankline': ({ children }) => null,
 
-    // ':para':({line,children})=>React.createElement(`p`,{'data-line':line, className:"line-src", id:`line-${line}`}, children),
-    ':para':({line,children})=>(<p data-line={line} className="line-src" id={`line-${line}`}>{children}</p>), 
-    
-    'pod': ({children})=><>{children}</>,
-    ':text' : ({value}) =>value,
-    'head':({level,children, line})=>React.createElement(`h${level}`,{'data-line':line, className:"line-src", id:`line-${line}`}, children),
-    'C<>':({children})=>createElement('code', {}, children),
-    ':list':({children, itemized})=>createElement(itemized ? 'ul' : 'ol',{}, children),
-    'item':({children})=>createElement('li', {}, children),
-    'L<>': ({content, children})=>createElement('a',{href:content[0]}, children),
-    'B<>': 'b',
-    'I<>': 'i',
-    'Image':({content, children})=>createElement('img',{src:content[0].value}),
-    'code': ({content, children})=>createElement('pre',{}, createElement('code',{},children)),
-    ':nested' : 'blockquote',
-    'nested' : 'blockquote',
-    ':table':'table',
-    ':table_row':'tr',
-    ':table_cell':'td',
-    ':verbatim' : ({value}) =>value,
-    'TITLE': ({level=1,children})=>React.createElement(`h${level}`,{}, children),
-    'DESCRIPTION': ({level=1,children})=>React.createElement(`h${level}`,{}, children),
+  // 'para':({line,children})=>React.createElement(`p`,{'data-line':line, className:"line-src", id:`line-${line}`}, children),
+  para: ({ line, children }) => (
+    <p data-line={line} className="line-src" id={`line-${line}`}>
+      {children}
+    </p>
+  ),
+
+  // ':para':({line,children})=>React.createElement(`p`,{'data-line':line, className:"line-src", id:`line-${line}`}, children),
+  ':para': ({ line, children }) => (
+    <p data-line={line} className="line-src" id={`line-${line}`}>
+      {children}
+    </p>
+  ),
+
+  pod: ({ children }) => <>{children}</>,
+  ':text': ({ value }) => value,
+  head: ({ level, children, line }) =>
+    React.createElement(`h${level}`, { 'data-line': line, className: 'line-src', id: `line-${line}` }, children),
+  'C<>': ({ children }) => createElement('code', {}, children),
+  ':list': ({ children, itemized }) => createElement(itemized ? 'ul' : 'ol', {}, children),
+  item: ({ children }) => createElement('li', {}, children),
+  'L<>': ({ content, children }) => createElement('a', { href: content[0] }, children),
+  'B<>': 'b',
+  'I<>': 'i',
+  Image: ({ content, children }) => createElement('img', { src: content[0].value }),
+  code: ({ content, children }) => createElement('pre', {}, createElement('code', {}, children)),
+  ':nested': 'blockquote',
+  nested: 'blockquote',
+  ':table': 'table',
+  ':table_row': 'tr',
+  ':table_cell': 'td',
+  ':verbatim': ({ value }) => value,
+  TITLE: ({ level = 1, children }) => React.createElement(`h${level}`, {}, children),
+  DESCRIPTION: ({ level = 1, children }) => React.createElement(`h${level}`, {}, children),
 }
 
-
-
-export function  AstToReact ({file,plugins={}}:{file:string, plugins?:any},...args) {
-    const content = file
-    // const fileNameExtension = path.parse(file).ext
-    // const type = 'pod6'
-    const   ast = useMemo( ()=>parse( content), [content])
-    const map2react = {
-        ...mapToReact,
-        ...plugins
+export function AstToReact({ file, plugins = {} }: { file: string; plugins?: any }, ...args) {
+  const content = file
+  // const fileNameExtension = path.parse(file).ext
+  // const type = 'pod6'
+  const ast = useMemo(() => parse(content), [content])
+  const map2react = {
+    ...mapToReact,
+    ...plugins,
+  }
+  const rules = {}
+  let i_key_i = 0
+  let mapByType = {}
+  const getIdForNode = ({ type = 'notype', name = 'noname' }) => {
+    const type_idx = `${type}_${name}`
+    if (!mapByType[type_idx]) {
+      mapByType[type_idx] = 0
     }
-    const rules = {}
-    let i_key_i = 0;
-    let mapByType= {}
-    const getIdForNode = ({type="notype",name="noname"}) =>{
-        const type_idx = `${type}_${name}`
-        if ( ! mapByType[type_idx]  ) { mapByType[type_idx] = 0}
-        ++mapByType[type_idx]
-        return `${type_idx}_${mapByType[type_idx]}`
+    ++mapByType[type_idx]
+    return `${type_idx}_${mapByType[type_idx]}`
+  }
+  Object.keys(map2react).map(ruleName => {
+    const makeElement = map2react[ruleName]
+
+    rules[ruleName] = (writer, processor) => (node, ctx, interator) => {
+      if (['Dia', 'para', ':para'].includes(ruleName)) {
+        console.log('Dia here!')
+        const { content, location, ...attr } = node
+        const key = node.type ? getIdForNode(node) : ++i_key_i
+        console.log({ key, node })
+        const children = content ? interator(content, { ...ctx }) : undefined
+        return makeElement({ ...attr, content, key, children, line: location?.start.line }, children)
+      }
+      if (!isValidElementType(makeElement)) {
+        throw new Error(`Bad React element for ${ruleName} rule `)
+      }
+      const { content, location, ...attr } = node
+      // console.log(JSON.stringify({node, location}, null,2))
+      // const key = location ? [node.type,++i_key_i].join('-') : ++i_key_i
+      const key = node.type ? getIdForNode(node) : ++i_key_i
+      const children = content ? interator(content, { ...ctx }) : undefined
+      return React.createElement(makeElement, { ...attr, content, key, line: location?.start.line }, children)
     }
-    Object.keys(map2react).map((ruleName)=>{ 
-        const makeElement = map2react[ruleName]
-        
-        rules[ruleName] = ( writer, processor )=>( node, ctx, interator )=>{
-            
-            if( ['Dia', 'para', ':para'].includes(ruleName)) {
-                console.log('Dia here!')
-                const { content, location, ...attr} = node
-                const key = node.type ? getIdForNode(node) : ++i_key_i
-                console.log({key, node})
-                const children = content ? interator(content, { ...ctx}) : undefined
-                return makeElement({ ...attr, content, key , children, line: location?.start.line}, children)
-            }
-            if (!isValidElementType(makeElement)) {
-                throw new Error(`Bad React element for ${ruleName} rule `)
-            }
-            const { content, location, ...attr} = node
-            // console.log(JSON.stringify({node, location}, null,2))
-            // const key = location ? [node.type,++i_key_i].join('-') : ++i_key_i
-            const key = node.type ? getIdForNode(node) : ++i_key_i
-            const children = content ? interator(content, { ...ctx}) : undefined
-            return React.createElement(makeElement,{ ...attr, content, key, line: location?.start.line }, children  )
-        }
-    })
-    const FirstLevel = ({node}) => {
-        const  res = useMemo( ()=>{
-        console.log("1")
-        return toAny({processor:1})
-        .use({'*:*':( writer, processor )=>( node, ctx, interator )=>{
-            console.warn(JSON.stringify(node,null,2))
+  })
+  const FirstLevel = ({ node }) => {
+    const res = useMemo(() => {
+      console.log('1')
+      return toAny({ processor: 1 })
+        .use({
+          '*:*': (writer, processor) => (node, ctx, interator) => {
+            console.warn(JSON.stringify(node, null, 2))
             // return undefined
             // return null
-            return React.createElement('code',{key:++i_key_i},`not supported node:${JSON.stringify(node,null,2)}`)
-        }})
+            return React.createElement(
+              'code',
+              { key: ++i_key_i },
+              `not supported node:${JSON.stringify(node, null, 2)}`,
+            )
+          },
+        })
         .use(rules)
-        .run(node) 
-        },[JSON.stringify(node)])
-        return res.interator
-    }
-    // console.log({ast})
-    // let reactResults = []
-    // if (Array.isArray(ast) ) {
-    //     for (let index = 0; index < ast.length; index++) {
-    //         const element = ast[index];
-    //         const comp = React.createElement(FirstLevel, {node:element})
-    //         reactResults.push(comp)
-    //         // console.log({element})
-    //     }
-    // }
-    // return <>{ast.map((node, i ) => (<FirstLevel id={i} node={node}/>))}</>
-    const  res = useMemo( ()=>
-        toAny({processor:1})
-        .use({'*:*':( writer, processor )=>( node, ctx, interator )=>{
-            console.warn(JSON.stringify(node,null,2))
-            // return undefined
-            return React.createElement('code',{key:++i_key_i},`not supported node:${JSON.stringify(node,null,2)}`)
-            if (node.children) interator(node.children, { ...ctx})
-        }})
-        .use(rules)
-        .run(ast)
-    ,[ast])
+        .run(node)
+    }, [JSON.stringify(node)])
     return res.interator
+  }
+  // console.log({ast})
+  // let reactResults = []
+  // if (Array.isArray(ast) ) {
+  //     for (let index = 0; index < ast.length; index++) {
+  //         const element = ast[index];
+  //         const comp = React.createElement(FirstLevel, {node:element})
+  //         reactResults.push(comp)
+  //         // console.log({element})
+  //     }
+  // }
+  // return <>{ast.map((node, i ) => (<FirstLevel id={i} node={node}/>))}</>
+  const res = useMemo(
+    () =>
+      toAny({ processor: 1 })
+        .use({
+          '*:*': (writer, processor) => (node, ctx, interator) => {
+            console.warn(JSON.stringify(node, null, 2))
+            // return undefined
+            return React.createElement(
+              'code',
+              { key: ++i_key_i },
+              `not supported node:${JSON.stringify(node, null, 2)}`,
+            )
+            if (node.children) interator(node.children, { ...ctx })
+          },
+        })
+        .use(rules)
+        .run(ast),
+    [ast],
+  )
+  return res.interator
 }
 
 // export function contentToReact( {content, type, plugins={}}:{content:string, type:"pod6" | "md", plugins?:any} ) {
@@ -133,9 +155,9 @@ export function  AstToReact ({file,plugins={}}:{file:string, plugins?:any},...ar
 //         ...plugins
 //     }
 //     const rules = {}
-//     Object.keys(map2react).map((ruleName)=>{ 
+//     Object.keys(map2react).map((ruleName)=>{
 //         const makeElement = map2react[ruleName]
-        
+
 //         rules[ruleName] = ( writer, processor )=>( node, ctx, interator )=>{
 //             if (!isValidElementType(makeElement)) {
 //                 throw new Error(`Bad React element for ${ruleName} rule `)
@@ -146,7 +168,7 @@ export function  AstToReact ({file,plugins={}}:{file:string, plugins?:any},...ar
 //             return React.createElement(makeElement,{ ...attr, content, key, line: location?.start.line }, children  )
 //         }
 //     })
-    
+
 //     const  res = toAny({processor:1})
 //         .use({'*:*':( writer, processor )=>( node, ctx, interator )=>{
 //             console.warn(JSON.stringify(node,null,2))
@@ -163,20 +185,19 @@ export function  AstToReact ({file,plugins={}}:{file:string, plugins?:any},...ar
 //     return res.interator
 // }
 
-
 // export default function  MdToReact ({source,plugins={}},...args) {
 
 //     const ast = useMemo( ()=>md2ast(source),[ source] )
-    
+
 //     const map2react = {
 //         ...mapToReact,
 //         ...plugins
 //     }
 //     // convert map to rules
 //     const rules = {}
-//     Object.keys(map2react).map((ruleName)=>{ 
+//     Object.keys(map2react).map((ruleName)=>{
 //         const makeElement = map2react[ruleName]
-        
+
 //         rules[ruleName] = ( writer, processor )=>( node, ctx, interator )=>{
 //             if (!isValidElementType(makeElement)) {
 //                 throw new Error(`Bad React element for ${ruleName} rule `)
@@ -187,8 +208,8 @@ export function  AstToReact ({file,plugins={}}:{file:string, plugins?:any},...ar
 //             return React.createElement(makeElement,{ ...attr, content, key }, children  )
 //         }
 //     })
-    
-//     const  res = 
+
+//     const  res =
 //         toAny({processor:1})
 //         .use({'*:*':( writer, processor )=>( node, ctx, interator )=>{
 //             console.log(JSON.stringify(node,null,2))
@@ -200,8 +221,3 @@ export function  AstToReact ({file,plugins={}}:{file:string, plugins?:any},...ar
 //         .run(ast)
 //     return res.interator
 // }
-
-
-
-
-
