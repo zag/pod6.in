e7c53e7adfc2a9132d8cb677a1bf065664403ed0
Alexander Zahatski <zag@cpan.org>
2026-03-19 06:57:35 +0100

    Update deps and fix portal resolutions

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,24 @@
 module.exports = {
   webpack: {
-    configure: {
-      resolve: { symlinks: false },
+    configure: (webpackConfig) => {
+      // Keep symlinks: false for portal resolutions
+      webpackConfig.resolve.symlinks = false
+
+      // Fix ESM modules that import 'react/jsx-runtime' without extension
+      // (needed for @uiw/react-codemirror and other CM6 packages)
+      webpackConfig.resolve.fullySpecified = false
+      if (!webpackConfig.module.rules) webpackConfig.module.rules = []
+      webpackConfig.module.rules.push({
+        test: /\.m?js/,
+        resolve: {
+          fullySpecified: false,
+        },
+      })
+
+      // Suppress source-map warnings from node_modules
+      webpackConfig.ignoreWarnings = [/Failed to parse source map/]
+
+      return webpackConfig
     },
   },
 }
